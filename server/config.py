765b57ad41d3704ee0854d08082f1c74855971f0
k-koech <koechkelvin97@gmail.com>
2023-07-05 15:03:24 +0300

    done

--- server/config.py
+++ server/config.py
@@ -8,7 +8,8 @@ from sqlalchemy import MetaData
 # app = Flask(__name__)
 app = Flask(__name__, static_url_path='',
                   static_folder='client/build',
-                  template_folder='frontend/dist')
+                  template_folder='client/build')
+                  
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kelvin:2j5VHLx66g1erbR0pPwnTDOGlPLKfy80@dpg-cic3bkp5rnuk9qatjeag-a.oregon-postgres.render.com/wattpad'
